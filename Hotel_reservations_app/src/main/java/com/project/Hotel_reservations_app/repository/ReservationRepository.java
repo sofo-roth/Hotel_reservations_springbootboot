68e7cf13b1b5a080b8376287d9eecf90b62be32c
sofo-roth <57064371+sofo-roth@users.noreply.github.com>
2024-01-13 15:29:31 +0200

    Delete Hotel_reservations_app directory

--- Hotel_reservations_app/src/main/java/com/project/Hotel_reservations_app/repository/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.Hotel_reservations_app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.Hotel_reservations_app.entity.Customer;
-
-public interface ReservationRepository extends JpaRepository<Customer,Integer>{
-	
-}
